ad161753ad84fe67a305a255ec019f047336143b
Jayant Kumar <jayantk237@gmail.com>
2023-03-09 21:22:58 +0530

    Expressjs setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('server...');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.json({
+        Message:'welcome to ticket support api'
+    })
+})
+
+app.listen(PORT,()=> console.log(`server started ${PORT}`))
