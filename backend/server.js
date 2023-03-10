25deb2ede73e0cba6a300808a489260a6b112ccc
Jayant Kumar <jayantk237@gmail.com>
2023-03-10 21:18:24 +0530

    connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
-const app = express()
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
 
+// connect db
+connectDB()
+
+const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.get('/',(req,res)=>{
