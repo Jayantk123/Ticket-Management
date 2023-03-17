e0d9adc2369c7f8f26117058b7cf285405bd53f5
Jayant Kumar <jayantk237@gmail.com>
2023-03-17 20:24:43 +0530

    home,register and login UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return <>
@@ -20,6 +21,7 @@ function App() {
 </div>
 
   </Router>
+  <ToastContainer/>
 
   
   </>
